74416918ea29b4965193baa88230f5081250b55b
RYANRANAUT <ryanranaut@gmail.com>
2021-07-27 20:30:08 +0530

    Initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "/firebase/app";
+
+const config = {
+  apiKey: "AIzaSyBZZ5eQwrJLikPl9ZeejsZWjyJM2IHjqBM",
+  authDomain: "engage-now-c8a2e.firebaseapp.com",
+  projectId: "engage-now-c8a2e",
+  storageBucket: "engage-now-c8a2e.appspot.com",
+  messagingSenderId: "298516182323",
+  appId: "1:298516182323:web:42209752c196fdfc98fa46",
+};
+
+const app = firebase.initializeApp(config);
