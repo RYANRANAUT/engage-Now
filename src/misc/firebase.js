9aede63848557c531c72be4bf2dc1ad22efb668b
RYANRANAUT <ryanranaut@gmail.com>
2021-07-28 19:12:10 +0530

    Added signin page and signin with fb/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "/firebase/app";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: "AIzaSyBZZ5eQwrJLikPl9ZeejsZWjyJM2IHjqBM",
@@ -10,3 +12,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
